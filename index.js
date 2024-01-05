ec5089ad40ad34f9dd2f5e361d68e0ea351429f0
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-05 21:09:31 +0530

    completed the styling part

---
+++ index.js
@@ -0,0 +1,10 @@
+function clock() {
+    var today = new Date();
+
+    var hourss =today.getHourss();
+    var minutes =today.getMinutes();
+    var seconds =today.getSeconds();    
+    let period = "AM";
+
+    document.
+}
