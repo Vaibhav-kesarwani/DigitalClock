6ee126ae8e8a8b0b7d30db7155c0bb3203392037
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2024-01-05 21:27:17 +0530

    make the clock logic also

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
+"use strict";
+
 function clock() {
-    var today = new Date();
+  var today = new Date();
 
-    var hourss =today.getHourss();
-    var minutes =today.getMinutes();
-    var seconds =today.getSeconds();    
-    let period = "AM";
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  let period = "AM";
 
-    document.
+  // set the time period (AM/PM)
+  if (hours >= 12) {
+    period = "PM";
+  }
+
+  // set the 12-hour clock format
+  hours = hours > 12 ? hours % 12 : hours;
+
+  // add the 0 for the value lower than 10
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+  document.querySelector(".period").innerHTML = period;
 }
+
+var updateClock = setInterval(clock, 1000);
+
+// get the date in js
+var today = new Date();
+
+const dayNumber = today.getDate();
+const year = today.getFullYear();
+const dayName = today.toLocaleString("default", { weekday: "long" });
+const monthName = today.toLocaleString("default", { month: "short" });
+
+document.querySelector(".month-name").innerHTML = monthName;
+document.querySelector(".day-name").innerHTML = dayName;
+document.querySelector(".day-number").innerHTML = dayNumber;
+document.querySelector(".year").innerHTML = year;
